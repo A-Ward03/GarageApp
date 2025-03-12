a5e1c52397ac3cbf8ad49d540023d80311d0b0b7
A-Ward03 <andrew.mac.ward03@gmail.com>
2025-03-12 16:11:59 +0000

    Abstract garage app from UI

--- src/main/java/services/GarageManager.java
+++ src/main/java/services/GarageManager.java
@@ -1,4 +1,42 @@
 package services;
 
+import model.Garage;
+import model.Vehicle;
+
+import java.util.Scanner;
+
 public class GarageManager {
+    private Garage myGarage;
+    private Scanner sc;
+
+    public  GarageManager() {
+        this.myGarage = new Garage();
+        this.sc = new Scanner(System.in);
+    }
+
+    public void addToGarage() {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("What is your car vin?");
+            String vin = sc.nextLine();
+            if (vin.isEmpty()) {
+                break;
+            }
+            Vehicle newVehicle = new Vehicle(vin);
+            myGarage.addVehicle(newVehicle);
+            System.out.println("Would you like to provide the make & model? (yes/no)");
+            String answer = sc.nextLine();
+            if (answer.toLowerCase().equals("yes")) {
+                System.out.println("What is the make & model of your vehicle?");
+                String makeAndModel = sc.nextLine();
+                String[] split = makeAndModel.split(" ", 2);
+                String make = split[0];
+                String model = split[1];
+                newVehicle.setMake(make);
+                newVehicle.setModel(model);
+            }
+            System.out.println("Vehicle added to garage!");
+        }
+        System.out.println(myGarage);
+    }
 }
