76bd15868084f4b6ffb985c6671cb77bd8948a50
A-Ward03 <andrew.mac.ward03@gmail.com>
2025-03-12 11:24:22 +0000

    Add class detail to vehicle

--- src/main/java/model/Vehicle.java
+++ src/main/java/model/Vehicle.java
@@ -1,5 +1,60 @@
 package model;
 
-public class Vehicle {
+import java.util.Date;
 
+public class Vehicle {
+    private final String vin;
+    private String currentRegistration;
+    private String Make;
+    private String model;
+    private String colour;
+    private Date manufactureYear;
+
+    public Vehicle(String vin) {
+        this.vin = vin;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public String getCurrentRegistration() {
+        return currentRegistration;
+    }
+
+    public void setCurrentRegistration(String currentRegistration) {
+        this.currentRegistration = currentRegistration;
+    }
+
+    public String getMake() {
+        return Make;
+    }
+
+    public void setMake(String make) {
+        Make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public Date getManufactureYear() {
+        return manufactureYear;
+    }
+
+    public void setManufactureYear(Date manufactureYear) {
+        this.manufactureYear = manufactureYear;
+    }
 }
